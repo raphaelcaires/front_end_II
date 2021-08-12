e871f60471eb7f7afa3fb69d84b48162208a578c
raphaelcaires <raphaelcaires@gmail.com>
2021-08-11 21:30:32 -0300

    v1.0.0

--- jo_ken_po/js/app.js
+++
@@ -1,9 +0,0 @@
-let situacao = confirm("Escolha sua jogada");
-
-if(situacao) {
-  // redireciona o usuário para a página de acesso permitido.
-  window.location.href="acessoPermitido.html";
-} else {
-  // redireciona o usuário para a página de acesso negado.
-  window.location.href="acessoNegado.html";
-}
